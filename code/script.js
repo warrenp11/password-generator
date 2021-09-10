57372996cf582a70d5c922b8904529b43c46d558
Peter <warrenp11@gmail.com>
2021-09-10 17:23:49 -0400

    variables for characters in password

--- code/script.js
+++ code/script.js
@@ -21,6 +21,15 @@ THEN the password is either displayed in an alert or written to the page
 */
 
 // Assignment code here
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+//var specialCharacters = [];
+var passwordLength = "";
+
+var generatePassword = function() {
+  window.prompt("Please select the length of your password. (Between 8 - 128 characters");
+};
 
 
 // Get references to the #generate element
