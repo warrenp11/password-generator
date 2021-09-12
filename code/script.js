de9532cc570be32779d602f7ebe78abb95ba3262
Peter <warrenp11@gmail.com>
2021-09-12 11:59:34 -0400

    able to choose password length and characters to use, but can't return random pass to browser

--- code/script.js
+++ code/script.js
@@ -1,35 +1,16 @@
-/*
-// **************************************************************************** //
-// ******************** ACCEPTANCE CRITERIA ******************** //
-
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-// **************************************************************************** //
-*/
 // ***********************
 // Assignment code 
 // ***********************
 
 // arrays to choose characters from
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialChar = ['!', '%', '&', ',', '*', '+', '-', '.', '/', '<', '>', '?','~'];
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var specialChar = '!@#$^&%*()+=-[]{}|:<>?,.';
+var number = '1234567890';
 
-var passwordChar = [];
+var confirmLength = "";
+var randomPassword = "";
+
 
 
 // prompt to confirm length of password (between 8-128)
@@ -80,38 +61,26 @@ function generatePassword() {
       );
   }
 
-  var passwordChar = [];
-
-  if (confirmLowerCase) {
-    passwordChar += lowercase;
-  }
-  if (confirmUpperCase) {
-    passwordChar += uppercase;
-  }
-  if (confirmNumber) {
-    passwordChar += number;
-  }
-  if (confirmSpecialChar) {
-    passwordChar += specialChar;
-  }
-
+  var passwordChar = "";
+  passwordChar += (confirmLowerCase ? lowercase : "");
+  passwordChar += (confirmUpperCase ? uppercase : "");
+  passwordChar += (confirmNumber ? number : "");
+  passwordChar += (confirmSpecialChar ? specialChar : "");
   console.log(passwordChar);
 
-  var randomPassword = "";
-
-  for (var i = 0; i < confirmLength; i++) {
-    randomPassword = randomPassword + passwordChar(Math.floor(Math.random) * passwordChar.length)
-    console.log(randomPassword);
-  }
-  return randomPassword;
-
+  randomPassword = password(confirmLength, passwordChar);
+  console.log(randomPassword);
 }
 
 
-// ***************************************
-// !!!!! DO NOT CODE BELOW HERE !!!!!!!!
-// ***************************************
+function password(l, passwordChar) {
+    var randomPassword = "";
+    for (var i =0; i < l; i++) {
+      randomPassword += passwordChar.charAt(Math.floor(Math.random() * passwordChar.lenth));
+    }   
+    return randomPassword;
 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
